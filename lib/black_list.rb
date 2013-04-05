3483564adf715e1ddb6e409010d7e82752dc7870
Shayan Guha <shayan@coliloquy.com>
2013-04-05 01:31:05 -0700

    Fix typo

--- lib/black_list.rb
+++ lib/black_list.rb
@@ -97,9 +97,9 @@ class BlackList
     def highlight_words!(text, kind, words)
       words.each do |word|
         if kind == :greedy
-          text.gsub!(/(#{word})+/i, '*\1*')
+          text.gsub!(/(#{word})+/i, '[*\1*]')
         else
-          text.gsub!(/\b(#{word})\b/i, '[*\1*]')
+          text.gsub!(/\b(#{word})\b/i, '*\1*')
         end
       end unless words.nil?
       text
